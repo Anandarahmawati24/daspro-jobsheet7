d4ff4ab36f7000ee2b19a11e18a3958ee5709a7b
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-10-12 22:00:10 +0700

    kode program perulangan while menampilkan nama mahasiswa perempuan

---
+++ Latihanmandiri24.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Latihanmandiri24{
+    public static void main(String[] args) {
+        Scanner input04 = new Scanner(System.in);
+        int jumlahMahasiswa= 30, jumlahPerempuan = 0;
+        char jenisKelamin;
+        String nama;
+
+        System.out.println(" Masukkan nama dan jenis kelamim Mahasiswa: ");
+        while ( jumlahPerempuan< jumlahMahasiswa){
+            System.out.print("Nama Mahasiswa:  ");
+            nama= input04.nextLine();
+
+            System.out.print(" Jenis Kelamin (L/P):  ");
+            jenisKelamin= input04.nextLine().charAt(0);
+
+            if (jenisKelamin == 'P' || jenisKelamin=='p') {
+            System.out.println(" Mahasiswa Perempuan: " +nama);
+            jumlahPerempuan++;
+           }  
+          }
+        }
+    }
