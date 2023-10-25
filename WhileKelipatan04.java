65c85842552b9347969922277114a44fd2c18d94
Anandarahmawati24 <144120040+Anandarahmawati24@users.noreply.github.com>
2023-10-25 20:58:43 +0700

    Update and rename WhileKelipatan4.java to WhileKelipatan04.java

--- WhileKelipatan04.java
+++ WhileKelipatan04.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class WhileKelipatan4 {
+public class WhileKelipatan04 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
